9648a8f98dfe2a9e4dcedeb7eab5974db3a1a2a0
Ryan <coap.ryanmontgomery@gmail.com>
2024-08-31 17:18:43 -0600

    complete

--- server/models.py
+++ server/models.py
@@ -1,24 +1,38 @@
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint, null
 from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if name == '':
+            raise ValueError('Author must have a name.')
+        elif Author.query.filter_by(name=name).first():
+            raise ValueError('Author must have a unique name.')
+        return name
+
+    @validates('phone_number')
+    def validate_phone(self, key, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError('Phone number must have exactly ten digits and contain only digits')
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.String)
@@ -27,7 +41,37 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators
+    @validates('title')
+    def validate_title(self, key, title):
+        if title == '':
+            raise ValueError('Post must have a title.')
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in title for phrase in clickbait_phrases):
+            raise ValueError('Title must contain one of the following phrases: "Won\'t Believe", "Secret", "Top", "Guess"')
+        return title
+
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Content must be 250 characters or more.')
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('Summary must not exceed 250 characters.')
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in  ['Fiction', 'Non-Fiction']:
+            raise ValueError('Category must be valid')
+        return category
+
+
+
 
 
     def __repr__(self):
